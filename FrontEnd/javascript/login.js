edbbbf58b0dcdefc6ccddc8ace7e73eb543aace9
Yoann Scherrer <yoannscherrer68@gmail.com>
2024-01-15 19:28:19 +0100

    Connexion utilisateur et sauvegarde Token

--- FrontEnd/javascript/login.js
+++ FrontEnd/javascript/login.js
@@ -1,3 +1,4 @@
+
 let email;
 let password;
 const btn = document.getElementById("button");
@@ -8,36 +9,35 @@ function getEmailAndPassword() {
         event.preventDefault();
         email = document.getElementById("email").value;
         password = document.getElementById("password").value; 
-        if (email == "" || password == ""){
-            alert("Erreur dans l’identifiant ou le mot de passe");
-        }
-        await getAPIresponse(email, password);     
+        await getAPIresponse(email, password);    
 })    
+    
 }
 
 async function getAPIresponse(email, password) {
     let login = {email, password};
     let login_json = JSON.stringify(login);
     console.log(login_json);
-    let response = await fetch(url_login, {
+    fetch(url_login, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: login_json
-    });
-    if (response.status == 404 || response.status == 401){
-        alert("Erreur dans l’identifiant ou le mot de passe");
-    }  
-    else if (response.status == 200){
-        document.location.href="index.html";
-        editor_mode();
-    }
+        body: login_json,
+        headers: { "Content-Type": "application/json" }
+    })
+    .then((response)=>response.json())
+    .then((data)=>{
+        localStorage.clear();
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("userId", data.userId);
+        console.log(data);
+        console.log(localStorage);
+        if (localStorage.getItem("token")==="undefined"){
+            alert("Erreur dans l’identifiant ou le mot de passe")
+        } 
+        else {
+            editor_mode(); 
+        }   
+    })
 }
 
-function editor_mode() {
-    let login_btn = document.getElementById("login_btn");
-    let logout_btn = document.getElementById("logout_btn");
-    login_btn.className = "hidden";
-    logout_btn.classList.remove("hidden");
-}
 
 getEmailAndPassword();
